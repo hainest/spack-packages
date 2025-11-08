38387804b21a98d0e2c2dbcb6659834e29c28814
Tim Haines <thaines.astro@gmail.com>
2025-11-08 15:28:29 -0600

    Extract threading variants

--- repos/spack_repo/builtin/packages/boost/boostorg/variants.py
+++ repos/spack_repo/builtin/packages/boost/boostorg/variants.py
@@ -156,6 +156,15 @@ def load():
         description="Build in debug mode",
     )
     variants.add(
+        "multithreaded",
+        description="Enable use of multiple threads",
+    )
+    variants.add(
+        "singlethreaded",
+        default=False,
+        description="Disable use of multiple threads",
+    )
+    variants.add(
         "taggedlayout",
         default=False,
         when="@1.40.0:",
