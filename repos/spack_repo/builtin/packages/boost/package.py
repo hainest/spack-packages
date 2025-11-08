08ceccbbbf6fb1f15657cad2d29efc634116f40b
Tim Haines <thaines.astro@gmail.com>
2025-11-08 15:20:38 -0600

    Extract visibility variant
    This also enforces the version constraint.

--- repos/spack_repo/builtin/packages/boost/package.py
+++ repos/spack_repo/builtin/packages/boost/package.py
@@ -231,15 +231,6 @@ class Boost(Package):
         "for building static libraries",
     )
 
-    # https://boostorg.github.io/build/manual/develop/index.html#bbv2.builtin.features.visibility
-    variant(
-        "visibility",
-        values=("global", "protected", "hidden"),
-        default="hidden",
-        multi=False,
-        description="Default symbol visibility in compiled libraries (1.69.0 or later)",
-    )
-
     depends_on("c", type="build")
     depends_on("cxx", type="build")
 
