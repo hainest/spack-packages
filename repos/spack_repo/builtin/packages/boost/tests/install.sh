9160bd29500089553cc8a14444c7ba2b72852daf
Tim Haines <thaines.astro@gmail.com>
2025-11-15 15:36:13 -0600

    Add basic installation tests

---
+++ repos/spack_repo/builtin/packages/boost/tests/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+declare build_jobs=16
+
+declare failed=0
+declare test_num=0
+
+function test_install () {
+  declare log_file="install_test_${test_num}.log"
+  echo -n "Install test ${test_num}: '$1'... " | tee ${log_file}
+  ((test_num++))
+  
+  if timeout 20m spack install --deprecated -v -j ${build_jobs} boost "$1" >>${log_file} 2>&1; then
+    echo "PASSED"
+    spack uninstall -y boost "$1" >/dev/null 2>&1
+    rm -f ${log_file}
+  else
+    RED='\033[0;31m'
+	NC='\033[0m' # No Color
+    echo -e "${RED}FAILED${NC}"
+    failed=1
+  fi
+}
+
+test_install ""
+test_install "%gcc"
+test_install "@develop %gcc"
+test_install "@develop %clang"
+test_install "+clanglibcpp~stacktrace %clang"
+
+# Layouts (can only be one type)
+for l in "versionedlayout" "taggedlayout"; do
+  # non-default build options
+  test_install "+debug+icu+singlethreaded+$l"
+done
+
+exit $failed
